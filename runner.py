a665b3607acb6e8362112a25b6d6a086d6e86025
Anokxz <snakshayan2@gmail.com>
2025-06-26 15:32:23 +0530

    updated max thread limit

--- runner.py
+++ runner.py
@@ -16,30 +16,44 @@ def run_testcase(command: str, testcase: str) -> dict :
         )
         duration = time.time() - start
         return {
-            "stdout": run_process.stdout.strip(),
-            "stderr": run_process.stderr.strip(),
-            "exit_code": run_process.returncode,
+            "stdout": run_process.stdout,
+            "stderr": run_process.stderr,
+            "return_code": run_process.returncode,
             "execution_time" : duration,
         }
     except subprocess.TimeoutExpired:
         return {
-            "stdout": "",
+            "stdout": run_process.stdout,
             "stderr": "Execution timed out",
-            "exit_code": 124,
+            "return_code": 124,
         }
     # For Debugging 
     except Exception as e:
         print(f"An error occurred: {e}")
         print(f"Type of error: {type(e)}")
         return {
-            "stdout": "",
+            "stdout": run_process.stdout,
             "stderr": str(e),
-            "exit_code": -1,
+            "return_code": -2,
         }
 
 def run_all_testcases(command: str, testcases: list) -> list :
-
+    max_thread_count = 50
     worker_count = len(testcases)
+    
+    # Test Case Limit
+    if (worker_count > max_thread_count):
+        return [{
+            "stdout": "",
+            """
+                This is acutally not a Standard Error,
+                It's Just Simple this way :) - Anokxz
+
+            """
+            "stderr": f"Number of testcase limit should be less than {max_thread_count}",
+            "return_code": -1,
+        }]
+
     with ThreadPoolExecutor(max_workers=worker_count) as executor:
         results = list(
             executor.map(
